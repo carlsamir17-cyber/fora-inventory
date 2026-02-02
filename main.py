3e2bbf7fe1ea9eb176b44fbe9f31f24329777532
carlsamir17-cyber <carlsamir17@gmail.com>
2026-02-03 00:40:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -193,7 +193,11 @@ def index():
     )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
